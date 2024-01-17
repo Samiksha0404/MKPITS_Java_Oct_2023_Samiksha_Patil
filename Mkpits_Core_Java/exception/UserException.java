bac8604da6d6bdb7ca3071e57973e55b5495ebb6
Samiksha0404 <samikshapatil040401@gmail.com>
2024-01-17 18:04:54 +0530

    d

---
+++ Mkpits_Core_Java/exception/UserException.java
@@ -0,0 +1,12 @@
+package com.mkpits.exception;
+
+public class UserException  extends Exception {
+
+	public UserException () {
+		
+	}
+	public String toString() {
+		return "Error in code";
+	}
+
+}
